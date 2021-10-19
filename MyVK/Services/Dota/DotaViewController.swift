650a29d0893759b5d63179eb1bbcce5a754ee7a4
ArturDokhno <artur.dohno@icloud.com>
2021-10-19 23:08:06 +0500

    Комит

--- MyVK/Services/Dota/DotaViewController.swift
+++ MyVK/Services/Dota/DotaViewController.swift
@@ -12,68 +12,62 @@ class DotaViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet var tableView: UITableView!
     
-    var heroes: Results<RealmHeroDota>?
-//    var heroes = [HeroStats]()
+    var heroes = [HeroStats]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        downloadJSON {
-//            self.tableView.reloadData()
-//        }
+        downloadJSON {
+            self.tableView.reloadData()
+        }
         
         tableView.delegate = self
         tableView.dataSource = self
 
-        loadFromRealm()
-    }
-    
-    private func loadFromRealm() {
-        heroes = try? RealmService.load(typeOf: RealmHeroDota.self)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return heroes?.count ?? 0
+        return heroes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        cell.textLabel?.text = heroes?[indexPath.row].localized_name.capitalized
+        cell.textLabel?.text = heroes[indexPath.row].localized_name.capitalized
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "showDetails", sender: self)
-//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetails", sender: self)
+    }
 
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if let destination = segue.destination as? HeroViewController {
-//            destination.hero = heroes[(tableView.indexPathForSelectedRow?.row)!]
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? HeroViewController {
+            destination.hero = heroes[(tableView.indexPathForSelectedRow?.row)!]
+        }
+    }
     
-//    func downloadJSON(completed: @escaping () -> ()) {
-//
-//        let url = URL(string: "https://api.opendota.com/api/heroStats")
-//
-//        URLSession.shared.dataTask(with: url!) { data, response, error in
-//
-//            if error == nil {
-//                do {
-//                self.heroes = try JSONDecoder().decode([HeroStats].self, from: data!)
-//
-//                    let realmHeros = self.heroes.map { RealmHeroDota(hero: $0)}
-//                    try RealmService.save(items: realmHeros)
-//
-//                    DispatchQueue.main.async {
-//                        completed()
-//                    }
-//                } catch {
-//                    print("JSON error")
-//                }
-//            }
-//        }.resume()
-//    }
+    func downloadJSON(completed: @escaping () -> ()) {
+
+        let url = URL(string: "https://api.opendota.com/api/heroStats")
+
+        URLSession.shared.dataTask(with: url!) { data, response, error in
+
+            if error == nil {
+                do {
+                self.heroes = try JSONDecoder().decode([HeroStats].self, from: data!)
+
+                    let realmHeros = self.heroes.map { RealmHeroDota(hero: $0)}
+                    try RealmService.save(items: realmHeros)
+
+                    DispatchQueue.main.async {
+                        completed()
+                    }
+                } catch {
+                    print("JSON error")
+                }
+            }
+        }.resume()
+    }
     
 }
 
