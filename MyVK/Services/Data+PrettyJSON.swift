d7fff12ae7418e11d1e6d1aa8c0e7f4c575faa1c
ArturDokhno <artur.dohno@icloud.com>
2021-11-01 19:45:44 +0500

    commit

--- MyVK/Services/Data+PrettyJSON.swift
+++ MyVK/Services/Data+PrettyJSON.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Data {
     
-    var prettyJSON: NSString? { /// NSString gives us a nice sanitized debugDescription
+    var prettyJSON: NSString? { 
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
               let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
               let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
